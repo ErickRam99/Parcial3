2db5c1908c2f06c06358b10244456c97936069d4
ErickRam99 <2521162019@mail.utec.edu.sv>
2023-04-19 21:36:03 -0600

    inicio

--- ErickParcial3/app/src/main/java/edu/sv/erickparcial/MainActivity.java
+++ ErickParcial3/app/src/main/java/edu/sv/erickparcial/MainActivity.java
@@ -11,34 +11,68 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import edu.sv.erickparcial.clases.guardarfrag;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView btnNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        btnNav = findViewById(R.id.btnNav);
 
-        public boolean onCreateOptionsMenu(Menu menu)
-        {
-            MenuInflater inflater = getMenuInflater();
-            inflater.inflate(R.menu.menuimaganes,menu);
-            return true ;
+        btnNav.setOnNavigationItemSelectedListener((BottomNavigationView.OnNavigationItemSelectedListener) navaListener);
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navaListener= new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment seleccionFrag= null;
+
+            switch (item.getItemId()){
+                case R.id.guardar:
+                    seleccionFrag = new guardarfrag();
+                    break;
+                case R.id.editar:
+                    seleccionFrag = new guardarfrag();
+                    break;
+                case R.id.eliminar:
+                    seleccionFrag = new guardarfrag();
+                    break;
+
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentCont, seleccionFrag).commit();
+            return true;
 
         }
 
 
 
+    };
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menuimaganes,menu);
+        return true ;
+
+    }
+
     public boolean onOptionsItemSelected(MenuItem items) {
 
-        //Intent intent = new Intent(this, Sumar.class);
+
 
         switch (items.getItemId()) {
 
             case R.id.opcion1:
                 Toast.makeText(this, "Selecciono la opcion 1" + 1, Toast.LENGTH_SHORT).show();
-                //startActivity(intent);
+
                 return true;
             case R.id.opcion2:
                 Toast.makeText(this, "Selecciono la opcion 2" + 1, Toast.LENGTH_SHORT).show();
