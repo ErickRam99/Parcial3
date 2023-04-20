d53b450c77420dd8ae0f3846c282e7348ca9be22
ErickRam99 <2521162019@mail.utec.edu.sv>
2023-04-19 19:34:54 -0600

    parcial

--- ErickParcial3/app/src/main/java/edu/sv/erickparcial/MainActivity.java
+++ ErickParcial3/app/src/main/java/edu/sv/erickparcial/MainActivity.java
@@ -1,8 +1,16 @@
 package edu.sv.erickparcial;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+        public boolean onCreateOptionsMenu(Menu menu)
+        {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.menuimaganes,menu);
+            return true ;
+
+        }
+
+
+
+    public boolean onOptionsItemSelected(MenuItem items) {
+
+        //Intent intent = new Intent(this, Sumar.class);
+
+        switch (items.getItemId()) {
+
+            case R.id.opcion1:
+                Toast.makeText(this, "Selecciono la opcion 1" + 1, Toast.LENGTH_SHORT).show();
+                //startActivity(intent);
+                return true;
+            case R.id.opcion2:
+                Toast.makeText(this, "Selecciono la opcion 2" + 1, Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.opcion3:
+                Toast.makeText(this, "Selecciono la opcion 3" + 1, Toast.LENGTH_SHORT).show();
+                return true;
+
+
+        }
+
+        return super.onOptionsItemSelected(items);
     }
 }
